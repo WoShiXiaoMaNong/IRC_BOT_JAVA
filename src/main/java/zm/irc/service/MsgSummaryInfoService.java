b7ea7649991ec2a5b2d3c0d6a00360ae5b66d254
zhongming139@126.com <qkhhje868587>
2022-01-02 17:07:04 +0800

    1.Init chat summary

--- src/main/java/zm/irc/service/MsgSummaryInfoService.java
+++ src/main/java/zm/irc/service/MsgSummaryInfoService.java
@@ -1,11 +1,23 @@
 package zm.irc.service;
 
 import org.apache.log4j.Logger;
+import zm.irc.dto.ChatSummary;
 
 public class MsgSummaryInfoService {
     private static final Logger log = Logger.getLogger(MsgSummaryInfoService.class);
 
-    public void queryMyChatInfo(String userName,String channel){
 
+    /**
+     * Query History chat record in the {channel} for {userName}
+     * @param userName
+     * @param channel
+     * @return
+     */
+    public ChatSummary queryMyChatInfo(String userName, String channel){
+        ChatSummary chatSummary = new ChatSummary(userName,channel);
+
+        log.error("tbd");
+
+        return chatSummary;
     }
 }
